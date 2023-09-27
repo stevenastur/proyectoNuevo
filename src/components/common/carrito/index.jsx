e0f2fa7a5c00e259e52c64dd2e98230093475100
Steven Astur <steven.astur@gmail.com>
2023-09-27 14:21:09 -0300

    icono carrito

--- src/components/common/carrito/index.jsx
+++ src/components/common/carrito/index.jsx
@@ -4,55 +4,66 @@ import { CartContext } from "../../../context/carrito";
 import { Button, Modal } from "react-bootstrap";
 
 const CarritoCompras = () => {
-  const carritoId = useId()
+  const carritoId = useId();
 
-  const { cart, setCart } = useContext(CartContext)
-  const [showModal, setShowModal] = useState(false)
-  const [montoTotal, setMontoTotal] = useState(0)
+  const { cart, setCart } = useContext(CartContext);
+  const [showModal, setShowModal] = useState(false);
+  const [montoTotal, setMontoTotal] = useState(0);
 
   const quantity = cart
     ? cart.reduce((acc, curr) => acc + curr.quantity, 0)
-    : 0
+    : 0;
   const totalPrecio = cart
     ? cart.reduce((acc, curr) => acc + curr.quantity * curr.precio, 0)
-    : 0
+    : 0;
 
-  const phoneNumber = "54 11 56533910"
-  const message = "¡Hola! Mi pedido es el siguiente.."
+  const phoneNumber = "54 11 56533910";
+  const message = "¡Hola! Mi pedido es el siguiente..";
 
-  const whatsappLink = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`
+  const whatsappLink = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(
+    message
+  )}`;
 
   const finalizarCompra = () => {
     const montoTotal = cart.reduce(
-      (acc, curr) => acc + curr.quantity * curr.precio, 0)
+      (acc, curr) => acc + curr.quantity * curr.precio,
+      0
+    );
 
     const pedido = cart.map((item) => {
-      return `${item.nombre} (${item.quantity} unidades) - monto: $${item.quantity * item.precio}` })
+      return `${item.nombre} (${item.quantity} unidades) - monto: $${
+        item.quantity * item.precio
+      }`;
+    });
 
-    const pedidoTexto = pedido.join("\n")
+    const pedidoTexto = pedido.join("\n");
 
-    const pedidoMessage = `¡Hola! Mi pedido es el siguiente:\n${pedidoTexto} \n\nMonto total del pedido: $${montoTotal}`
+    const pedidoMessage = `¡Hola! Mi pedido es el siguiente:\n${pedidoTexto} \n\nMonto total del pedido: $${montoTotal}`;
 
-    const whatsappLink = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(pedidoMessage)}`
+    const whatsappLink = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(
+      pedidoMessage
+    )}`;
 
-    window.open(whatsappLink, "_blank")
+    window.open(whatsappLink, "_blank");
 
-    setCart([])
+    setCart([]);
 
     const total = cart.reduce(
-      (acc, curr) => acc + curr.quantity * curr.precio, 0)
-    setMontoTotal(total)
+      (acc, curr) => acc + curr.quantity * curr.precio,
+      0
+    );
+    setMontoTotal(total);
 
-    setShowModal(true)
-  }
+    setShowModal(true);
+  };
   const closeModal = () => {
-    setShowModal(false)
-  }
+    setShowModal(false);
+  };
 
   return (
     <>
       <label htmlFor={carritoId} className="cart-button">
-        Carrito de Compras
+        <span class="material-symbols-outlined">shopping_cart_checkout</span>
       </label>
       <input id={carritoId} type="checkbox" hidden />
 
@@ -106,7 +117,7 @@ const CarritoCompras = () => {
         </ul>
       </aside>
     </>
-  )
-}
+  );
+};
 
-export { CarritoCompras }
+export { CarritoCompras };
