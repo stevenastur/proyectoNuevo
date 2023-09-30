0b747b3a27311d366157986aafd9ec97414a0d21
Steven Astur <steven.astur@gmail.com>
2023-09-30 00:07:51 -0300

    falta ver img

--- src/components/common/item/index.jsx
+++ src/components/common/item/index.jsx
@@ -1,7 +1,13 @@
-import { Card, Button, Row, Col } from "react-bootstrap";
+import { Card, Button, Col, Image } from "react-bootstrap";
 import { useCart } from "../hook/carrito";
 import "./style.scss";
 import { AddToCartButton } from "../boton/add-to-card";
+import campo from "../../../assets/campo.jpg";
+import ciabatta from "../../../assets/ciabatta.jpg";
+import burga from "../../../assets/burga.jpg";
+import pizzaCebolla from "../../../assets/pizzaCebolla.jpg";
+import lactal from "../../../assets/lactal.jpg";
+import lactalArtesano from "../../../assets/lactalArtesano.jpg";
 
 const Item = ({ id, nombre, precio, verProducto, textButton }) => {
   const { addToCart, cart, removerItem, getQuantityById } = useCart();
@@ -12,20 +18,24 @@ const Item = ({ id, nombre, precio, verProducto, textButton }) => {
 
   const quantity = getQuantityById(id);
 
+  const productImages = {
+    campo: campo,
+    ciabatta: ciabatta,
+    burga: burga,
+    pizzaCebolla: pizzaCebolla,
+    lactal: lactal,
+    lactalArtesano: lactalArtesano,
+  };
+
   return (
     <>
       <Card key={id} className="tarjeta-individual">
-        <Card.Body className="cuerpo-card">
-          <Col className="columna-card">
-            <Card.Title>{nombre}</Card.Title>
-          </Col>
-          <Col className="columna-card">
-            <Card.Text>$ {precio}</Card.Text>
-            <Button variant="secondary" onClick={verProducto}>
-              {textButton}
-            </Button>
+        <Card.Body className="columna-card">
+          <Col className="imagen">
+            <Image src={productImages[nombre]} rounded />
           </Col>
           <Col className="columna-card-agregar">
+            <Card.Title>{nombre}</Card.Title>
             <AddToCartButton
               product={{ id, nombre, precio }}
               addToCart={addToCart}
@@ -33,6 +43,12 @@ const Item = ({ id, nombre, precio, verProducto, textButton }) => {
               getQuantityById={getQuantityById}
               quantity={quantity}
             />
+          </Col>
+          <Col className="columna-card-agregar">
+            <Card.Text className="precio">$ {precio}</Card.Text>
+            <Button variant="secondary" onClick={verProducto} className="boton-ver">
+              {textButton}
+            </Button>
           </Col>
         </Card.Body>
       </Card>
